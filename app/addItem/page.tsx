7f9414fa2044640d5feaced88eddea29819083e8
pelegsch666 <peleg.mazuz@gmail.com>
2023-06-24 13:04:32 +0300

    recoil added

--- app/addItem/page.tsx
+++ app/addItem/page.tsx
@@ -8,6 +8,7 @@ const AddItems = (props: Props) => {
   const [item, setItem] = useState<ItemState>({
     name: '',
     itemDescription: '',
+    catalogNumber: 0,
     itemType: 'fruit',
     date: arrangeDate().toLocaleDateString('en-GB'),
   });
@@ -24,11 +25,11 @@ const AddItems = (props: Props) => {
   };
 
   return (
-    <div className="flex flex-col p-24 ">
+    <div className="flex flex-col p-24 justify-center items-center gap-5">
       <h1>Adding Items</h1>
       <form
         onSubmit={onSubmit}
-        className="flex flex-col ">
+        className="flex flex-col items-center justify-center gap-5">
         <label>
           {' '}
           Product Name:
@@ -42,9 +43,9 @@ const AddItems = (props: Props) => {
             }}
           />
         </label>
-        <label>
-          {' '}
+        <label className="mb-3" htmlFor="itemDescription">
           itemDescription:
+          </label>
           <textarea
             className="text-black"
             name="name"
@@ -53,7 +54,7 @@ const AddItems = (props: Props) => {
               setItem({ ...item, itemDescription: e.target.value });
             }}
           />
-        </label>
+        
         <label>
           {' '}
           Item Type:
@@ -77,6 +78,19 @@ const AddItems = (props: Props) => {
             name="date"
             onChange={onChangeOfDate}
           />
+        </label>
+        <label>
+            {' '}
+            Catalog Number:
+            <input
+                className="text-black"
+                type="number"
+                name="catalogNumber"
+                required
+                
+                onChange={e => { setItem({ ...item, catalogNumber: Number(e.target.value) }) }}/>
+               
+
         </label>
         <button className="border-red">Send</button>
       </form>
--- utils/helpers/function.ts
+++ utils/helpers/function.ts
@@ -1,5 +1,13 @@
 export const arrangeDate = () => {
-    const dateNow = new Date();
-    const aWeekFromNow = new Date(dateNow.getTime() + 7 * 24 * 60 * 60 * 1000);
-    return aWeekFromNow;
-}
+  const dateNow = new Date();
+  const aWeekFromNow = new Date(dateNow.getTime() + 7 * 24 * 60 * 60 * 1000);
+  return aWeekFromNow;
+};
+
+export const checkIfNumberIsPositive = (value: number) => {
+  if (value < 0) {
+    throw new Error('Value must be greater than 0');
+  }
+
+  return true ;
+};
